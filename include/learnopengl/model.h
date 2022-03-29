a146e0cd6a9cceee7167f80aa9bf0e1126775757
rexi12345 <relja.pesic@gmail.com>
2022-03-29 09:20:38 +0200

    drugi main

--- include/learnopengl/model.h
+++ include/learnopengl/model.h
@@ -241,7 +241,7 @@ unsigned int TextureFromFile(const char *path, const string &directory, bool gam
         glGenerateMipmap(GL_TEXTURE_2D);
 
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE :GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
